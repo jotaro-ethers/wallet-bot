b048e0b35a28cc946690896aea897f3b6b114e9c
jotaro <nqtien0507@gmail.com>
2023-12-04 17:42:39 +0700

    covert to TS

--- src/bot.ts
+++ src/bot.ts
@@ -1,21 +1,11 @@
-// bot/bot.js
 import { Telegraf } from 'telegraf';
-// const { start } = require('./commands/start');
-import start from './commands/start';
-// const { MONGODB_URI } = require('../config');
-// const { connect } = require('../database');
-
-// connect();
-// const  Config = require('./config/config.js');
+import {start} from './commands/start';
 import {Config} from './config/config';
 const bot = new Telegraf(Config.TELEGRAM_TOKEN);
 
-// // Commands
-// bot.command('start', start);
-
-// // Add more commands and functionality as needed
-// bot.launch().then(() => {
-//     console.log('Bot is running');
-//   }).catch((err) => {
-//     console.error('Error starting bot:', err);
-//   });
+bot.command('start', start);
+bot.launch().then(() => {
+    console.log('Bot is running');
+  }).catch((err) => {
+    console.error('Error starting bot:', err);
+  });
--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,10 +1,9 @@
 import { Context } from 'telegraf';
 
 const start = async function start(ctx: Context) {
-  const userId = ctx.message.from.id;
+  const userId = ctx.message?.from.id;
   console.log(ctx.message);
-  const userName = ctx.message.from.username;
-
+  const userName = ctx.message?.from.username;
   ctx.reply(`Hello ${userName}! Your user ID is ${userId}. Welcome to the bot!`);
 }
-export default start
+export {start};
