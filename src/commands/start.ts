4539516a536647c7e372bbb0865e503eb4a3dbda
jotaro <nqtien0507@gmail.com>
2023-12-04 17:10:16 +0700

    ts

---
+++ src/commands/start.ts
@@ -0,0 +1,10 @@
+import { Context } from 'telegraf';
+
+const start = async function start(ctx: Context) {
+  const userId = ctx.message.from.id;
+  console.log(ctx.message);
+  const userName = ctx.message.from.username;
+
+  ctx.reply(`Hello ${userName}! Your user ID is ${userId}. Welcome to the bot!`);
+}
+export default start
