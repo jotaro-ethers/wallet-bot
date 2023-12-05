8d60461b71df1882b21043252bb9b039c3729b90
jotaro <nqtien0507@gmail.com>
2023-12-05 16:28:46 +0700

    [update Balances]

--- src/database/models/balance.ts
+++ src/database/models/balance.ts
@@ -6,13 +6,12 @@ const BalanceSchema:Schema = new Schema({
         required:true,
         unique:true
     },
-    balance:{
-        type:Number,
+    balances:[{
+        tokenAddress:{ type:String},
+        tokenName:{ type:String},
+        tokenSymbol:{ type:String},
+        balance:{ type:[Number]},
         required:true,
-    },
-    timestamp:{
-        type:Number,
-        required:true,
-    },
+    }],
 });
 export const balanceModel = mongoose.model("balance",BalanceSchema);
