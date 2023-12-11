8e481ccbcde89387dad69f1d9b804aae8f6cf66f
ntloi2706 <ntloi120202@gmail.com>
2023-12-11 08:34:23 +0700

    === Init Feature/transfer === init feature transfer

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
-// config.js
-// require('dotenv').config();
+// config.ts
 import dotenv from 'dotenv';
+
 dotenv.config();
+
 export const Config ={
   TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || 'your_telegram_bot_token',
   MONGODB_URI: process.env.MONGODB_URI || 'your_mongodb_connection_string',
