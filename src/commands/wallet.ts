de4e4a259fb9e69925985311613a06ce1a7dc004
ntloi2706 <ntloi120202@gmail.com>
2023-12-19 02:30:16 +0700

    === Update Feature/transfer no6 ===
    Done transfer for address

--- src/commands/wallet.ts
+++ src/commands/wallet.ts
@@ -1,81 +1,86 @@
-import { Context } from 'telegraf';
-import { Markup } from 'telegraf';
-import { userModel } from '../database/models/user';
-import * as utilsdata from '../helpers/utilsdata';
-import * as utils from '../helpers/utils';
-import * as path from 'path';
-import dayjs from 'dayjs';
-import 'dayjs/locale/en'; 
-const assetsFolderPath = path.join(__dirname, '..', 'assets');
+import { Context } from "telegraf";
+import { Markup } from "telegraf";
+import { userModel } from "../database/models/user";
+import * as utilsdata from "../helpers/utilsdata";
+import * as utils from "../helpers/utils";
+import * as path from "path";
+import dayjs from "dayjs";
+import "dayjs/locale/en";
+const assetsFolderPath = path.join(__dirname, "..", "assets");
 export async function wallet(ctx: Context) {
-    if(!utils.timeout(ctx.message?.date as number)){    
-    const buttonLink = Markup.button.callback('Link wallet', 'buttonLink');
+  if (!utils.timeout(ctx.message?.date as number)) {
+    const buttonLink = Markup.button.callback("Link wallet", "buttonLink");
     var ButtonsWallets = [[buttonLink]];
     const user = await userModel.findOne({ userId: ctx.from?.id });
-        if (user) {
-            for (var i = 0; i < user.wallets.length; i++) {
-                var walletButton = [
-                    Markup.button.callback(user.wallets[i].address, 'wallet/' + user.wallets[i].address),
-                    Markup.button.callback('❌','wallet/del/' + user.wallets[i].address)
-                ];
-                ButtonsWallets.push(walletButton);
-
-            }
-            ctx.reply('Your wallet list here, select the wallet you want to use or link a new one!', Markup.inlineKeyboard(ButtonsWallets));
-        } else {
-            ctx.reply('Your wallet list, select the wallet you want to use or link a new one!', Markup.inlineKeyboard(ButtonsWallets));
-            console.log("User not found");
-        }
+    if (user) {
+      for (var i = 0; i < user.wallets.length; i++) {
+        var walletButton = [
+          Markup.button.callback(
+            user.wallets[i].address,
+            "wallet/" + user.wallets[i].address
+          ),
+          Markup.button.callback("❌", "wallet/del/" + user.wallets[i].address),
+        ];
+        ButtonsWallets.push(walletButton);
+      }
+      ctx.reply(
+        "Your wallet list here, select the wallet you want to use or link a new one!",
+        Markup.inlineKeyboard(ButtonsWallets)
+      );
+    } else {
+      ctx.reply(
+        "Your wallet list, select the wallet you want to use or link a new one!",
+        Markup.inlineKeyboard(ButtonsWallets)
+      );
+      console.log("User not found");
     }
-}
-
-export async function Delwallet(ctx: Context){
-    const data = (ctx.callbackQuery as any)?.data;
-    console.log(data)
-    if (data){
-        const prefix = 'wallet/del/'
-        const extractedData = data.substring(prefix.length);
-        const userId = ctx.from?.id;
-        utilsdata.deleteWallet(userId,extractedData);
-        ctx.reply('Deleted wallet: ' + extractedData);
-
-    }else{
-    console.error('Callback query is undefined');
   }
 }
 
-export async function trackWallet(ctx: Context){
-    const data = (ctx.callbackQuery as any)?.data;
-    if (data){
-        const time = dayjs().locale(`${ctx.from?.language_code}}`);
-        const timeFormatted = time.format('YYYY-MM-DD HH:mm:ss');
-        const prefix = 'wallet/'
-        const extractedData = data.substring(prefix.length);
-        var loadingMessage = await ctx.replyWithHTML('Loading...');
-        const {tokens, totalRemain} = await utils.trackWallet(extractedData);
-        const tokensJson = JSON.parse(JSON.stringify(tokens));
-        var htmlText = `<pre>Address: ${extractedData}\n---------------------------------------------------\n`
-        for (const token of tokensJson) {
-            htmlText += `${token.name} ($${token.symbol}) : ${token.balance} | USD : ${token.remain} \n📈 Price: ${token.price}`
-            if (token.priceChangePercentage as number > 0){
-                htmlText += ` 🟢 ${token.priceChange} | +${token.priceChangePercentage}%\n`;
-            }
-            else{
-                htmlText += ` 🔴 ${token.priceChange} | ${token.priceChangePercentage}%\n`;
-            }
-        }
-        htmlText+=`💰Total Balance: ${totalRemain} USD</pre>\n`
-        htmlText+=`<i>⌚️-------${timeFormatted}-------⌚️\n ℹ️ The bot only displays the top tokens that have been verified by Vicscan </i>\n📢 VictelPay - Advertise with us @jotaro_ne\n🔎 View your wallet on explorer <a href="https://vicscan.xyz/address/${extractedData}">👇</a>\n`
+export async function Delwallet(ctx: Context) {
+  const data = (ctx.callbackQuery as any)?.data;
+  console.log(data);
+  if (data) {
+    const prefix = "wallet/del/";
+    const extractedData = data.substring(prefix.length);
+    const userId = ctx.from?.id;
+    utilsdata.deleteWallet(userId, extractedData);
+    ctx.reply("Deleted wallet: " + extractedData);
+  } else {
+    console.error("Callback query is undefined");
+  }
+}
 
-        await ctx.telegram.editMessageText(
-            ctx.chat?.id,
-            loadingMessage.message_id,
-            undefined,
-            htmlText,
-            {parse_mode: "HTML"});
-            
-        
-    }else {
-    console.error('Callback query is undefined');
+export async function trackWallet(ctx: Context) {
+  const data = (ctx.callbackQuery as any)?.data;
+  if (data) {
+    const time = dayjs().locale(`${ctx.from?.language_code}}`);
+    const timeFormatted = time.format("YYYY-MM-DD HH:mm:ss");
+    const prefix = "wallet/";
+    const extractedData = data.substring(prefix.length);
+    var loadingMessage = await ctx.replyWithHTML("Loading...");
+    const { tokens, totalRemain } = await utils.trackWallet(extractedData);
+    const tokensJson = JSON.parse(JSON.stringify(tokens));
+    var htmlText = `<pre>Address: ${extractedData}\n---------------------------------------------------\n`;
+    for (const token of tokensJson) {
+      htmlText += `${token.name} ($${token.symbol}) : ${token.balance} | USD : ${token.remain} \n📈 Price: ${token.price}`;
+      if ((token.priceChangePercentage as number) > 0) {
+        htmlText += ` 🟢 ${token.priceChange} | +${token.priceChangePercentage}%\n`;
+      } else {
+        htmlText += ` 🔴 ${token.priceChange} | ${token.priceChangePercentage}%\n`;
+      }
+    }
+    htmlText += `💰Total Balance: ${totalRemain} USD</pre>\n`;
+    htmlText += `<i>⌚️-------${timeFormatted}-------⌚️\n ℹ️ The bot only displays the top tokens that have been verified by Vicscan </i>\n📢 VictelPay - Advertise with us @jotaro_ne\n🔎 View your wallet on explorer <a href="https://vicscan.xyz/address/${extractedData}">👇</a>\n`;
+
+    await ctx.telegram.editMessageText(
+      ctx.chat?.id,
+      loadingMessage.message_id,
+      undefined,
+      htmlText,
+      { parse_mode: "HTML" }
+    );
+  } else {
+    console.error("Callback query is undefined");
   }
 }
--- src/helpers/transferBot.ts
+++ src/helpers/transferBot.ts
@@ -1,23 +1,62 @@
 import { Markup, Telegraf, Context } from "telegraf";
+import { userModel } from "../database/models/user";
+
+interface InfTransfer {
+  fromAdd: string;
+  toAdd: string;
+  amount: string;
+  tokenAddr: string;
+  type: string;
+}
+
+interface Action {
+  messageId: number;
+  title: string;
+  menu: any;
+  titleInput: string;
+}
 
 class TransferBotHelper {
   readonly #bot: Telegraf;
 
-  #messageId: number = 0;
-  #preTitleMess: string = "";
-  #preMenu: any = null;
-  #titleInput: string = "";
+  #listAction: Action[] = [];
 
-  #fromAdd: string = "";
-  #toAdd: string = "";
+  #infoTransfer: InfTransfer = {
+    fromAdd: "",
+    toAdd: "",
+    amount: "",
+    tokenAddr: "",
+    type: "",
+  };
 
   constructor(bot: Telegraf) {
     this.#bot = bot;
   }
 
-  private createTypeTransferButtons(): any {
-    // create buttons
-    return Markup.inlineKeyboard([
+  // save action
+  private performAction(action: Action): void {
+    this.#listAction.push(action);
+  }
+
+  // get pre action
+  private getPreviousAction(): Action {
+    return (
+      this.#listAction.pop() || {
+        messageId: 0,
+        title: "",
+        menu: null,
+        titleInput: "",
+      }
+    );
+  }
+
+  // get current action
+  private getCurrentAction(): Action {
+    return this.#listAction[this.#listAction.length - 1];
+  }
+
+  private createTypeTransferButtons(): Object[] {
+    return [
       [
         Markup.button.callback("Transfer with user name", "userNameBtn"),
         Markup.button.callback("Transfer with address", "addBtn"),
@@ -26,68 +65,173 @@ class TransferBotHelper {
         Markup.button.callback("Transfer with OneID", "ondIdBtn"),
         Markup.button.callback("Cancel transfer", "cancelBtn"),
       ],
-    ]);
+    ];
+  }
+
+  private createBtxBackAndCancel(): Object[] {
+    return [
+      Markup.button.callback("<< Back >>", "backBtx"),
+      Markup.button.callback("<< Cancel transfer >>", "cancelBtn"),
+    ];
+  }
+
+  private createConfirmTransfer(): Object[] {
+    return [
+      [Markup.button.callback("Sureeeeee", "sure")],
+      this.createBtxBackAndCancel(),
+    ];
+  }
+
+  private createTypeToken(): Object[] {
+    return [
+      [
+        Markup.button.callback("Native", "nativeBtn"),
+        Markup.button.callback("Token", "tokenBtn"),
+        Markup.button.callback("Nft", "nftBtn"),
+      ],
+      this.createBtxBackAndCancel(),
+    ];
+  }
+
+  private async createBtxAddressUser(ctx: Context): Promise<any> {
+    const user = await userModel.findOne({ userId: ctx.from?.id }).lean();
+    if (user && user.wallets && user.wallets.length > 0) {
+      const buttons = [
+        user.wallets.map((wallet: any) =>
+          Markup.button.callback(wallet.address, `transfer-${wallet.address}`)
+        ),
+      ];
+      buttons.push(this.createBtxBackAndCancel());
+      return buttons;
+    } else {
+      console.log("User not found or no wallets available");
+      return [];
+    }
+  }
+
+  // create menu
+  private async menu(
+    title: string,
+    menu: any,
+    titleInput: string = "",
+    ctx: Context
+  ): Promise<void> {
+    const message = await ctx.reply(title, Markup.inlineKeyboard(menu));
+
+    this.performAction({
+      messageId: message.message_id,
+      title: title,
+      menu: menu,
+      titleInput: titleInput,
+    });
+    console.log(this.#listAction);
   }
 
   private handleUserNameBtn(): void {
     this.#bot.action("userNameBtn", async (ctx) => {
       await ctx.answerCbQuery();
-      await ctx.deleteMessage(this.#messageId); // destroy action other buttons
+      const currentAction = this.getCurrentAction();
+      await ctx.deleteMessage(currentAction?.messageId);
 
-      // save menu
-      this.#preTitleMess = "Please select a transfer type:";
-      this.#preMenu = this.createTypeTransferButtons();
-
-      const newMessage = "Enter the telegram user name you want to transfer";
-      const newButtons = Markup.inlineKeyboard([
-        Markup.button.callback("<< Back to list type transfer", "backBtx"),
-        Markup.button.callback("Cancel transfer", "cancelBtn"),
-      ]);
-
-      this.#titleInput = "InputUserName";
-
-      const message = await ctx.reply(newMessage, newButtons); // new buttons
-      this.#messageId = message.message_id;
+      await this.menu(
+        "Enter the address you want to transfer",
+        this.createBtxBackAndCancel(),
+        "InputAddress",
+        ctx
+      );
     });
   }
 
-  private handleAddBtn(): void {
+  private handleAddrBtn(): void {
     this.#bot.action("addBtn", async (ctx: Context) => {
       await ctx.answerCbQuery();
-      await ctx.deleteMessage(this.#messageId); // destroy action other buttons
+      const currentAction = this.getCurrentAction();
+      await ctx.deleteMessage(currentAction?.messageId);
 
-      // save menu
-      this.#preTitleMess = "Please select a transfer type:";
-      this.#preMenu = this.createTypeTransferButtons();
+      await this.menu(
+        "Enter the address you want to transfer",
+        this.createBtxBackAndCancel(),
+        "InputAddress",
+        ctx
+      );
+    });
+  }
 
-      const newMessage = "Enter the address you want to transfer";
-      const newButtons = Markup.inlineKeyboard([
-        Markup.button.callback("<< Back to list type transfer", "backBtx"),
-        Markup.button.callback("Cancel transfer", "cancelBtn"),
-      ]);
-      this.#titleInput = "InputAddress";
+  private handleMenuTypeToken(): void {
+    this.#bot.action("nativeBtn", async (ctx: Context) => {
+      await ctx.answerCbQuery();
+      const currentAction = this.getCurrentAction();
+      await ctx.deleteMessage(currentAction?.messageId);
 
-      const message = await ctx.reply(newMessage, newButtons); // new buttons
-      this.#messageId = message.message_id;
+      this.#infoTransfer.type = "native";
+
+      this.menu(
+        "Enter amount you want to transfer",
+        this.createBtxBackAndCancel(),
+        "InputAmount",
+        ctx
+      );
+    });
+  }
+
+  private handleBtxAddressUser(): void {
+    this.#bot.action(/transfer-\d+/, async (ctx) => {
+      await ctx.answerCbQuery();
+
+      const currentAction = this.getCurrentAction();
+      await ctx.deleteMessage(currentAction?.messageId);
+
+      const walletId = (ctx.callbackQuery as any)?.data.split("-")[1];
+      await ctx.reply(`Wallet ID: ${walletId}`);
+
+      this.#infoTransfer.toAdd === ""
+        ? (this.#infoTransfer.toAdd = walletId)
+        : (this.#infoTransfer.fromAdd = walletId);
+
+      await this.menu(
+        "You sure for transfer ?",
+        this.createConfirmTransfer(),
+        undefined,
+        ctx
+      );
     });
   }
 
   private handleInputText(): void {
     this.#bot.on("text", async (ctx) => {
       const userInput = ctx.message.text;
-      const chatId = ctx.chat?.id;
-      console.log(userInput);
-      console.log(this.#preTitleMess);
+
       if (userInput) {
-        switch (this.#titleInput) {
+        const currentAction = this.getCurrentAction();
+        switch (currentAction.titleInput) {
+          //todo
           case "InputUserName":
-            // todo: handle message for transfer address
             await ctx.reply("HIHI");
             break;
+          //todo
           case "InputAddress":
-            // todo: handle message for transfer username
-            await ctx.reply("HAHA");
+            this.#infoTransfer.toAdd = userInput;
+            // delete message current
+            await ctx.deleteMessage(currentAction?.messageId);
+            await this.menu(
+              "Please choose type token for transfer: ",
+              this.createTypeToken(),
+              undefined,
+              ctx
+            );
             break;
+          //todo
+          case "InputAmount":
+            this.#infoTransfer.amount = userInput;
+            // delete message current
+            await ctx.deleteMessage(currentAction?.messageId);
+            const button = await this.createBtxAddressUser(ctx);
+            await this.menu(
+              "Please choose address of your to transfer:",
+              button,
+              undefined,
+              ctx
+            );
           default:
             break;
         }
@@ -97,39 +241,53 @@ class TransferBotHelper {
 
   sendTransferOptions(): void {
     this.#bot.command("transfer", async (ctx) => {
-      const message = await ctx.reply(
+      this.menu(
         "Please select a transfer type:",
-        this.createTypeTransferButtons()
+        this.createTypeTransferButtons(),
+        undefined,
+        ctx
       );
 
-      this.#messageId = message.message_id;
       this.handleUserNameBtn();
-      this.handleAddBtn();
-
-      // handle input message
+      this.handleAddrBtn();
       this.handleInputText();
+      this.handleMenuTypeToken();
+      this.handleBtxAddressUser();
 
-      // handle button cancel
       this.#bot.action("cancelBtn", async (ctx) => {
         await ctx.answerCbQuery();
-        await ctx.deleteMessage(this.#messageId); // destroy action other buttons
+        const currentAction = this.getPreviousAction();
+        await ctx.deleteMessage(currentAction?.messageId);
+
         await ctx.reply("Cancel transfer success");
-        this.#preTitleMess = "";
-        this.#preMenu = null;
+
+        this.#listAction = [];
       });
 
-      // handle button back
       this.#bot.action("backBtx", async (ctx) => {
         await ctx.answerCbQuery();
-        await ctx.deleteMessage(this.#messageId); // destroy action current button
 
-        // this.#preTitleMess = "";
-        // this.#preMenu = null;
+        const currentAction = this.getPreviousAction();
+        // delete action current
+        await ctx.deleteMessage(currentAction?.messageId);
 
-        // previous menu
-        this.#messageId = (
-          await ctx.reply(this.#preTitleMess, this.#preMenu)
-        ).message_id;
+        const preAction = this.getPreviousAction();
+        await this.menu(
+          preAction?.title,
+          preAction?.menu,
+          preAction?.titleInput,
+          ctx
+        );
+      });
+
+      this.#bot.action("sure", async (ctx) => {
+        await ctx.answerCbQuery;
+
+        const currentAction = this.getPreviousAction();
+        await ctx.deleteMessage(currentAction?.messageId);
+        this.#listAction = [];
+
+        await ctx.reply("Wait for transfer");
       });
     });
   }
